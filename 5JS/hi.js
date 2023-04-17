4bcdec80212b0afd2cb72986f5dfa16031f45667
tamir haspel <haspeltamir@gmail.com>
2023-04-17 16:26:49 +0300

    finish

---
+++ 5JS/hi.js
@@ -0,0 +1,15 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var remaningTime=-age;
+        ageDays=365*remaningTime;    
+        ageWeeks=52*remaningTime;    
+        ageMonths=12*remaningTime;    
+        console.log("You have "+ageDays+" days, "+ageWeeks+" weeks, and "+ageMonths+" months left.")
+        
+    /*************Don't change the code below**********/
+    }
+    
+    
